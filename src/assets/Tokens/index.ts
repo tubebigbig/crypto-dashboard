9782c97d860edad7773ef11e61164c313ce051dc
Victor <tubebigbig@gmail.com>
2024-08-14 11:21:06 +0800

    feat: configure crypto token list

---
+++ src/assets/Tokens/index.ts
@@ -0,0 +1,7 @@
+import ethTokenList from "@assets/Tokens/eth.json";
+import sepoliaTokenList from "@assets/Tokens/sepolia.json";
+
+export default {
+  mainnet: ethTokenList,
+  sepolia: sepoliaTokenList,
+};
